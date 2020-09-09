8869931812a9aa54c18e23ae8a8bee680ef422fc
hangooksaram <hyunjae1995@gmail.com>
2020-09-09 23:31:31 +0900

    no changed file

--- models/musics.js
+++
@@ -1,8 +0,0 @@
-const { TextareaAutosize } = require("@material-ui/core");
-
-module.exports = (sequelize, DataTypes) => 
-{ return sequelize.define('musicdata', { 
-    title: {type: DataTypes.STRING(20), allowNull: false,}, 
-    genre: { type: DataTypes.STRING(100), allowNull: false, }, 
-    rate: { type: DataTypes.INTEGER(10), allowNull: false, }, }); 
-}
