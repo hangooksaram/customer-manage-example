8495be74543794940b56944ba2cae28416b9da4c
hangooksaram <hyunjae1995@gmail.com>
2020-10-23 23:51:56 +0900

    mui?

---
+++ music/src/components/themepractice.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ThemePractice = (theme) => {
+    return (
+        <div className = {theme}>this is themePractice</div>
+    )
+}
+
+export default ThemePractice
---
+++ music/src/css/mainpage-css.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  background : red;
+`
+
+const Title= styled.h1`
+  background : yellow;
+`
+const textStyle = {
+  color : 'blue'
+}
+
+export {Wrapper, Title, textStyle};
